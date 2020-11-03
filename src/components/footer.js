4341f3c50a8adffdf7b017960badb33aa244914d
tauqeerawan <tauqeer.insta@gmail.com>
2020-11-03 19:38:27 +0400

    fix: footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,27 +4,23 @@ import SimpleGrid from "@chakra-ui/core/dist/SimpleGrid"
 import NewsletterForm from "./newsletter-form"
 import Icon from "@chakra-ui/core/dist/Icon"
 import Link from "@chakra-ui/core/dist/Link"
-import { FiFacebook, FiTwitter, FiLinkedin } from 'react-icons/fi'
+import { FiFacebook, FiTwitter, FiLinkedin } from "react-icons/fi"
 
 const Footer = () => {
   return (
-    <SimpleGrid
-      columns={1}
-      backgroundColor={"gray.700"}
-      background={"linear-gradient(to bottom right, #2e3192 30%, purple)"}
-    >
+    <SimpleGrid columns={1}>
       <Box d={"flex"} justifyContent={"center"} p={4}>
         <Box position={"relative"} top={-50}>
           <NewsletterForm />
         </Box>
       </Box>
       <Box d={"flex"} justifyContent={"center"} p={4} color={"white"}>
-        <Box textAlign={'center'}>
+        <Box textAlign={"center"}>
           <Box mb={2}>Follow us</Box>
           <Box>
-            <Icon as={FiFacebook} fontSize={'2xl'} mr={2} />
-            <Icon as={FiTwitter} fontSize={'2xl'} mr={2} />
-            <Icon as={FiLinkedin} fontSize={'2xl'} mr={2} />
+            <Icon as={FiFacebook} fontSize={"2xl"} mr={2} />
+            <Icon as={FiTwitter} fontSize={"2xl"} mr={2} />
+            <Icon as={FiLinkedin} fontSize={"2xl"} mr={2} />
           </Box>
         </Box>
       </Box>
@@ -45,7 +41,7 @@ const Footer = () => {
         </Box>
       </Box>
       <Box d={"flex"} justifyContent={"center"} p={4} color={"white"} mb={4}>
-        All rights reserved © 2020 by Devplode.com
+        All rights reserved © 2020 by educode.com
       </Box>
     </SimpleGrid>
   )
